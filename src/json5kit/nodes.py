5710982cd69875410cf8a31359a16b77ccfcb9a6
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2022-09-11 04:03:46 +0530

    Reimplement JSON5 nodes

--- src/json5kit/nodes.py
+++ src/json5kit/nodes.py
@@ -1,124 +1,179 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, field
-from typing import Literal, Protocol
+from typing import Protocol
+
+# So, here's the new idea: Trivia nodes.
+#
+# Basically everything that is valid JSON data will be part of the node's attributes,
+# but everything else (such as whitespace, newlines, comments, trailing commas) will be
+# in a list of what I'm calling "trivia nodes". Every single json data node (booleans,
+# numbers, etc.) will have a property `trailing_trivia_nodes`, which will list all the
+# trivia nodes found after the said node.
+#
+# There's just one slight thing to take care of: Since there can be whitespace, comments
+# etc. in the beginning of an array/object/file before any of the content, they require
+# different trivia node handling, they'll have a `leading_trivia_nodes` property too.
+# These three nodes I'll be calling "scope" nodes.
 
 
-class Json5Data(Protocol):
-    @property
-    def source(self) -> str:
+class Json5Node(Protocol):
+    """Sets the expectation from a JSON5 node: be able to convert back to source."""
+
+    def to_json5(self) -> str:
+        ...
+
+    def to_json(self) -> str:
         ...
 
 
-@dataclass
-class Json5Comment:
-    comment: str
-    whitespace_before_comment: str
+class Json5Primitive:
+    """Base class for primitive JSON types such as booleans, null, integers etc."""
 
-    @property
-    def value(self) -> str:
-        return self.whitespace_before_comment + self.comment
-
-
-@dataclass
-class Json5Null:
-    source = "null"
-
-
-@dataclass
-class Json5Boolean:
-    value: bool
-
-    @property
-    def source(self) -> str:
-        return "true" if self.value else "false"
-
-
-@dataclass
-class Json5String:
-    source: str
-    value: str
-    quotes: Literal['"', "'"]
-
-
-@dataclass
-class Json5Number:
-    source: str
-    value: float
-
-
-@dataclass
-class Json5ArrayMember:
-    value: Json5Value
-    comma: bool
-    whitespace_after_comma: str
-    trailing_comments: list[Json5Comment] = field(default_factory=list)
-
-    @property
-    def source(self) -> str:
-        value = self.value.to_json5()
-        if self.comma:
-            value += ","
-
-        return (
-            value
-            + self.whitespace_after_comma
-            + "".join("//" + comment.value for comment in self.trailing_comments)
-        )
-
-
-@dataclass
-class Json5Array:
-    items: list[Json5ArrayMember]
-    trailing_whitespace: str = ""
-    trailing_comments: list[str] = field(default_factory=list)
-
-    @property
-    def source(self) -> str:
-        return (
-            "["
-            + "".join(item.source for item in self.items)
-            + "]"
-            + self.trailing_whitespace
-        )
-
-
-@dataclass
-class Json5Value:
-    data: Json5Data
-    whitespace_before: str
-    whitespace_after: str
-    trailing_comments: list[Json5Comment] = field(default_factory=list)
+    def __init__(
+        self,
+        source: str,
+        value: object,
+        trailing_trivia_nodes: list[Json5Trivia],
+    ) -> None:
+        self.source = source
+        self.value = value
+        self.trailing_trivia_nodes = trailing_trivia_nodes
 
     def to_json5(self) -> str:
-        return (
-            self.whitespace_before
-            + self.data.source
-            + self.whitespace_after
-            + "".join(self.trailing_comments)
+        return self.source + "".join(
+            trivia.source for trivia in self.trailing_trivia_nodes
+        )
+
+    def to_json(self) -> str:
+        return self.source
+
+
+class Json5Null(Json5Primitive):
+    def __init__(self, trailing_trivia_nodes: list[Json5Trivia]) -> None:
+        super().__init__(
+            source="null",
+            value=None,
+            trailing_trivia_nodes=trailing_trivia_nodes,
         )
 
 
-# NOTE: so I think associating comments with Json5Value is a fine decision.
-# Most of the comments can end up being associated with a value. The few exceptions
-# being ones at the beginning of an array/objecy, and comments after the colon in an
-# object. But those can be added to Json5Array and Json5Object, it's alright.
-#
-# Also note that no special distinction is needed for a comment on the same line as a
-# Json5value compared to one that's on the next line, as that's done via the
-# whitespace_after field having a newline or not having a newline in it.
-#
-# There's still a lot of places where comments can go, for eg:
-#
-#    // comment
-#    // comment
-# [  // comment
-#    // comment
-#    "a"  // comment
-#         // comment
-#     ,   // comment
-#     // comment
-# ] // comment
-#   // comment
-#
-# So it needs more proper speccing.
+class Json5Boolean(Json5Primitive):
+    def __init__(
+        self,
+        source: str,
+        value: bool,
+        trailing_trivia_nodes: list[Json5Trivia],
+    ) -> None:
+        super().__init__(source, value, trailing_trivia_nodes)
+
+
+class Json5Integer(Json5Primitive):
+    def __init__(
+        self,
+        source: str,
+        value: int,
+        trailing_trivia_nodes: list[Json5Trivia],
+    ) -> None:
+        super().__init__(source, value, trailing_trivia_nodes)
+
+
+class Json5Container:
+    """
+    Base class for "container nodes", i.e. nodes that contain other nodes.
+
+    This distinction is required because container nodes can have leading trivia
+    nodes, while primitive nodes like ints and booleans cannot.
+
+    Examples of container nodes include files, arrays and objects.
+    """
+
+    def __init__(
+        self,
+        leading_trivia_nodes: list[Json5Trivia],
+        trailing_trivia_nodes: list[Json5Trivia],
+    ) -> None:
+        self.leading_trivia_nodes = leading_trivia_nodes
+        self.trailing_trivia_nodes = trailing_trivia_nodes
+
+    def to_json5(self) -> str:
+        """Converts the node back to its original source."""
+        raise NotImplementedError
+
+    def to_json(self) -> str:
+        raise NotImplementedError
+
+
+class Json5Array(Json5Container):
+    def __init__(
+        self,
+        leading_trivia_nodes: list[Json5Trivia],
+        trailing_trivia_nodes: list[Json5Trivia],
+    ) -> None:
+        super().__init__(leading_trivia_nodes, trailing_trivia_nodes)
+        self.members: list[Json5Node] = []
+
+    def to_json5(self) -> str:
+        """Converts the node back to its original source."""
+        return (
+            "["
+            + "".join(trivia.source for trivia in self.leading_trivia_nodes)
+            + ",".join(member.to_json5() for member in self.members)
+            + "".join(trivia.source for trivia in self.trailing_trivia_nodes)
+            + "]"
+        )
+
+    def to_json(self) -> str:
+        """Converts the node back to its original source."""
+        return "[" + ",".join(member.to_json() for member in self.members) + "]"
+
+
+class Json5Object(Json5Container):
+    def __init__(
+        self,
+        leading_trivia_nodes: list[Json5Trivia],
+        trailing_trivia_nodes: list[Json5Trivia],
+    ) -> None:
+        super().__init__(leading_trivia_nodes, trailing_trivia_nodes)
+        self.data: dict[Json5Primitive, Json5Primitive] = {}
+
+    def to_json5(self) -> str:
+        """Converts the node back to its original source."""
+        return (
+            "{"
+            + "".join(trivia.source for trivia in self.leading_trivia_nodes)
+            + ",".join(
+                f"{key.to_json5()}:{value.to_json5()}"
+                for key, value in self.data.items()
+            )
+            + "".join(trivia.source for trivia in self.trailing_trivia_nodes)
+            + "}"
+        )
+
+    def to_json(self) -> str:
+        """Converts the node back to its original source."""
+        return (
+            "{"
+            + ",".join(
+                f"{key.to_json()}:{value.to_json()}" for key, value in self.data.items()
+            )
+            + "}"
+        )
+
+
+class Json5Trivia:
+    """Base class for "trivial" information like whitespace, newlines and comments."""
+
+    def __init__(self, source: str) -> None:
+        self.source = source
+
+
+class Json5Comment(Json5Trivia):
+    """JSON5 single line comments, eg. `// foo`."""
+
+
+class Json5Whitespace(Json5Trivia):
+    """Any run of continuous whitespace characters in a JSON5 file."""
+
+
+class Json5Newline(Json5Trivia):
+    """Newline character in a JSON5 file."""
